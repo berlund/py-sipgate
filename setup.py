5f560f1db1d92b842157751490cc0937383eeb4a
Lars Engel <lars.engel@gmail.com>
2017-11-30 11:18:13 +0100

    Including Readme.md and using it as only readme

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 
 # Utility function to read the README file, borrowed from
 # https://github.com/Innovailable/pysipgate/blob/master/setup.py
-#def read(fname):
-#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='sipgate',
+    python_requires='>=3',
     version='0.0.1',
     description='A client for the Sipgate REST API',
-    long_description='More about that later',
+    long_description=read('README.md'),
     license='MIT',
     author='Lars Engel',
     author_email='lars.engel@gmail.com',
@@ -20,5 +21,4 @@ setup(
     install_requires=['requests'],
     py_modules=["sipgate"],
     provides=["sipgate"]
-
 )
