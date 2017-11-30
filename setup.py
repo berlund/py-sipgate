ca07196d455710eabbfd0e0bfa6e28addf9efc44
Lars Engel <lars.engel@gmail.com>
2017-11-30 09:44:27 +0100

    Restructuring module

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ from setuptools import setup
 
 # Utility function to read the README file, borrowed from
 # https://github.com/Innovailable/pysipgate/blob/master/setup.py
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+#def read(fname):
+#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='sipgate',
     version='0.0.1',
     description='A client for the Sipgate REST API',
-    long_description=read('README.md'),
+    long_description='More about that later',
     license='MIT',
-    packages=['sipgate'],
     author='Lars Engel',
     author_email='lars.engel@gmail.com',
     keywords=['example'],
-    url='https://github.com/berlund/py-sipgate'
+    url='https://github.com/berlund/py-sipgate',
+    install_requires=['requests'],
+    py_modules=["sipgate"],
+    provides=["sipgate"]
+
 )
--- sipgate.py
+++ sipgate.py
@@ -5,11 +5,12 @@ This client comes with some restrictions:
 - Currently only the endpoints devices/ and devices/{deviceid} are supported
 """
 import requests
-from model import Device
+
+__version__ = '0.0.1'
 
 BASE_URL = "https://api.sipgate.com/v1/"
 
-class SipgateApi(object):
+class Client(object):
     """The client"""
 
     def __init__(self, username, password):
@@ -40,3 +41,28 @@ class SipgateApi(object):
         else:
             response.raise_for_status()
 
+class Device(object):
+
+    def __init__(self, device_id, alias, online):
+        self._id = device_id
+        self._alias = alias
+        self._online = online
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def alias(self):
+        return self._alias
+
+    @property
+    def online(self):
+        return self._online
+
+    @staticmethod
+    def from_json(json_object):
+        return Device(
+            json_object.get('id'), 
+            json_object.get('alias'),
+            json_object.get('online'))
